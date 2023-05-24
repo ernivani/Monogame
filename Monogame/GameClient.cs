12d6e78b9e3ff28219b576c6af6780399277df1e
Thomas Lindeker <tlindeker0@gmail.com>
2023-05-25 00:40:17 +0200

    initial commit

---
+++ Monogame/GameClient.cs
@@ -0,0 +1,75 @@
+using Lidgren.Network;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame
+{
+    public class GameClient : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private NetClient _client;
+
+        public GameClient()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            _client = new NetClient(new NetPeerConfiguration("game"));
+            _client.Start();
+            _client.Connect("213.32.89.28", 25565);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            NetIncomingMessage message;
+            while ((message = _client.ReadMessage()) != null)
+            {
+                switch (message.MessageType)
+                {
+                    case NetIncomingMessageType.Data:
+                        // Traitement des données reçues du serveur
+                        // ...
+                        break;
+                }
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            // Dessiner le jeu
+            // ...
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new GameClient())
+                game.Run();
+        }
+    }
+}
