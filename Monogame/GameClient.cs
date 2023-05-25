ba95f24d14b3d283a80f7bc3e72fc777d3ccc94f
Thomas Lindeker <tlindeker0@gmail.com>
2023-05-25 22:55:07 +0200

    move handle client

--- Monogame/GameClient.cs
+++ Monogame/GameClient.cs
@@ -12,20 +12,33 @@ namespace Monogame
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private NetClient _client;
-        private Dictionary<long, int> playerPositions = new Dictionary<long, int>();
+        private Dictionary<long, Vector2> playerPositions = new Dictionary<long, Vector2>();
         private Texture2D playerTexture;
+        private long myUniqueId;
+        private Vector2 myPosition;
+        private Matrix viewMatrix;
+        private KeyboardState oldState;
 
         public GameClient()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
+            _graphics.PreferredBackBufferWidth = 800;  // Set this to your desired width
+            _graphics.PreferredBackBufferHeight = 600;  // Set this to your desired height
+            // allow the window to be resized
+            Window.AllowUserResizing = true;
+            Window.ClientSizeChanged += OnResize;
+
+            _graphics.ApplyChanges();
             _client = new NetClient(new NetPeerConfiguration("game"));
             _client.Start();
             _client.Connect("213.32.89.28", 9999);
+            myUniqueId = _client.UniqueIdentifier;
 
             base.Initialize();
         }
@@ -49,26 +62,36 @@ namespace Monogame
                 {
                     case NetIncomingMessageType.Data:
                         long id = message.ReadInt64();
-                        int pos = message.ReadInt32();
+                        float x = message.ReadFloat();
+                        float y = message.ReadFloat();
+                        Vector2 pos = new Vector2(x, y);
                         playerPositions[id] = pos;
+                        if (id == myUniqueId) myPosition = pos;
                         System.Diagnostics.Debug.WriteLine($"Received position {pos} for player {id}");
                         break;
                 }
             }
 
-            var keyboardState = Keyboard.GetState();
-            if (keyboardState.IsKeyDown(Keys.Left))
+            var newState = Keyboard.GetState();
+            if (newState.IsKeyDown(Keys.Left))
                 _client.SendMessage(_client.CreateMessage("MOVE_LEFT"), NetDeliveryMethod.ReliableOrdered);
-            else if (keyboardState.IsKeyDown(Keys.Right))
+            if (newState.IsKeyDown(Keys.Right))
                 _client.SendMessage(_client.CreateMessage("MOVE_RIGHT"), NetDeliveryMethod.ReliableOrdered);
-            else if (keyboardState.IsKeyDown(Keys.Up))
+            if (newState.IsKeyDown(Keys.Up))
                 _client.SendMessage(_client.CreateMessage("MOVE_UP"), NetDeliveryMethod.ReliableOrdered);
-            else if (keyboardState.IsKeyDown(Keys.Down))
+            if (newState.IsKeyDown(Keys.Down))
                 _client.SendMessage(_client.CreateMessage("MOVE_DOWN"), NetDeliveryMethod.ReliableOrdered);
-            
-            else if (keyboardState.IsKeyDown(Keys.Escape))
+
+            oldState = newState;
+
+            if (newState.IsKeyDown(Keys.Escape))
                 Exit();
             
+            // Focus the view on the player
+            viewMatrix = Matrix.CreateTranslation(
+                -myPosition.X + _graphics.PreferredBackBufferWidth / 2,
+                -myPosition.Y + _graphics.PreferredBackBufferHeight / 2,
+                0);
 
             base.Update(gameTime);
         }
@@ -77,19 +100,22 @@ namespace Monogame
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            _spriteBatch.Begin();
+            _spriteBatch.Begin(SpriteSortMode.Deferred, null, null, null, null, null, viewMatrix);
             foreach (var player in playerPositions)
             {
-                _spriteBatch.Draw(playerTexture, new Vector2(player.Value * 10, 0), Color.White);
+                _spriteBatch.Draw(playerTexture, player.Value * 10, Color.White);
                 System.Diagnostics.Debug.WriteLine($"Drawing player {player.Key} at position {player.Value * 10}");
             }
-            // For debugging: Draw a player at a known position
-            _spriteBatch.Draw(playerTexture, new Vector2(100, 100), Color.Red);
             _spriteBatch.End();
 
             base.Draw(gameTime);
         }
+        
+        public void OnResize(Object sender, EventArgs e)
+        {
+            _graphics.PreferredBackBufferWidth = Window.ClientBounds.Width;
+            _graphics.PreferredBackBufferHeight = Window.ClientBounds.Height;
+            _graphics.ApplyChanges();
+        }
     }
-
-    
 }
